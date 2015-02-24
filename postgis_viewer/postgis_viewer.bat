9e251bd23b9294d933bbc27381f1194786255270
Germán Carrillo <geotux_tuxman@linuxmail.org>
2015-02-24 13:21:26 -0500

    Added changelog and .bat files.

---
+++ postgis_viewer/postgis_viewer.bat
@@ -0,0 +1,8 @@
+@echo off
+set OSGEO4W_ROOT=C:\OSGeo4W
+PATH=%OSGEO4W_ROOT%\bin;%PATH%
+for %%f in (%OSGEO4W_ROOT%\etc\ini\*.bat) do call %%f
+set PYTHONPATH=C:\OSGeo4W\apps\qgis\python
+set PATH=C:\OSGeo4W\apps\qgis\bin;%PATH%
+
+start /B python "C:/Archivos de programa/PostgreSQL/8.4/bin\postgis_viewer\postgis_viewer.py" %*
